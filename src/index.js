5e121baa90d3047aacb45626be1c892c8ee3a638
Pandahome <Capeqianteo@gmail.com>
2019-12-28 21:45:32 +0100

    Premier setup garphql

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import hi from "./page_1.js" ;
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express'; 
+import cors from 'cors';
 console.log("Pinguin");
 console.log(process.env.MY_SECRET);
 let msg = require("./page_1.js");
@@ -12,21 +13,40 @@ let msg = require("./page_1.js");
 const app = express();
 const schema = gql`
   type Query {
+    users: [User!]
     me: User
+    user(id : ID!) : User
   }
   type User {
     username: String!
+    id :ID!
   }
 `;
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+const me = users[1];
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
     me: () => {
-      return {
-        username: 'Robin Wieruch',
-      };
+      return me;
+    },
+    user: (parent, {id}) => {
+      return users[id];
     },
   },
 };
+ 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
@@ -35,6 +55,6 @@ server.applyMiddleware({ app, path: '/graphql' });
 app.listen({ port: 8000 }, () => {
   console.log('Apollo Server on http://localhost:8000/graphql');
 });
-
+app.use(cors());
 
 
