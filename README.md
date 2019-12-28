f8840ada88c933033fd14e101bfe31cdcedbe0e3
Pandahome <Capeqianteo@gmail.com>
2019-12-29 00:15:03 +0100

    Debut du readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# test_p_1
+## Test d'install et de setup d'Apollo/Express
+Avec l'aide du Tuto suivant: "https://www.robinwieruch.de/graphql-apollo-server-tutorial#apollo-server-setup-with-express"
+
+
+# Creation de l'environement et lancement du projet
+
+0. Prérequis
+-Node.js
+
+1. Initialisation du projet avec Npm.
+- On commence par creer notre repo avec gitHub. 
+- Ensuite on lance un "npm init -y" pour commencer notre projet
+- On cree ensuite le repo src dans le root de notre app "mkdir src" 
+- On cree aussi le fichier index.js apres avoir accédé au dossier "cd src && touch index.js"
+- On effectue un console.log("Ok tout roule") dans index.js pour verifier que tous fonctionne lors du lancement de node
+- On lance node src/index.js.
+- On nous renvoie ("Ok tout roule").
+- On rajoute dans la partie script du fichier package.json "start": "node src/index.js", pour initialiser notre projet avec "npm start"
+
+2. Install de nodeamon
+(FrameWork permettant de lancer en continu notre serveur sans devoir faire npm start a chaque fois qu'un changement est effectué.)
+- On install nodeamon avec la commande "npm install nodemon --save-dev"
+- On change ensuite dans le fichier package.json la commande start dans script par "nodemon src/index.js"
+- On change le console.log en ("Ca roule toujours")
+
+3. Install de Babel
+(Babel est un compileur javascript qui permet d'utiliser toutes les fonctionalité de celui ci car node ne possede pas toutes les fonctionalité)
+- On installe Babel avec la commande suivante "npm install @babel/core @babel/node --save-dev"
+- On change ensuite dans le fichier package.json la commande start dans script par "nodemon --exec babel-node src/index.js"
+- On installe ensuite les preset de Babel  "npm install @babel/preset-env --save-dev"
+- On cree un fichier .babelrc "touch .babelrc"
+- On ajoute le script suivant dans le fichier precedent :
+...
+{
+  "presets": [
+    "@babel/preset-env"
+  ]
+}
+...
--- src/index.js
+++ src/index.js
@@ -18,23 +18,31 @@ let msg = require("./page_1.js");
 // On lance le serveur express en l'initalisant dans une constante
 const app = express();
 // On construit ici le schema de notre "base de donnée"
-//Le schema est un liste de definitions de type. On commence par le type Query puis on remplis les different champ(User) de la DB
+//Le schema est un liste de definitions de type. On commence par le type Query puis on remplis les differents champs(User,Message) de la DB
 //gql´...` est le format GraphQl de DB
 //Pour le premier "users : [User!]" On cree un type users que tous les users possede par defaut et qui est obligatoire
 //Pour le second "me :User" On definis un objet me de typer User. Dans ce ca il a le "scalar type(champ classique)" username(string) et id(ID)
 //'!' veut dire que ces champs ne peuvent pas etre null 
+// Message 
 const schema = gql`
   type Query {
     users: [User!]
     me: User
     user(id : ID!) : User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
   type User {
     username: String!
     id :ID!
   }
+  type Message{
+    id: ID!
+    text: String!
+  }
 `;
-//Le user indique le squellette de notre DB, ici on a types qui sont username et ID
+//User est une table qui est composée des champ Id et usernam
 let users = {
   1: {
     id: '1',
@@ -45,8 +53,19 @@ let users = {
     username: 'Dave Davids',
   },
 };
+//message est une table qui est composée des champ Id et usernam
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+  },
+};
 // On defenis une variable "me" qui prend comme champ le premier user comme champ
-const me = users[1];
+//const me = users[1]; Plus besoin de l'utiliser car il est init dans l'argument contexte(3eme argument) de Apollo serveur 
 // Le resolvers permet d'aller chercher les donnée dans la DB c'est ici que tous les query devront etre effectuée
 const resolvers = {
 
@@ -55,16 +74,25 @@ const resolvers = {
     users: () => {
       return Object.values(users);
     },
-    // On renvoie toutes les données de 'me' 
-    me: () => {
+    // On renvoie toutes les données de 'me'
+    // Le 3 eme 'me' argument viens de context.
+    me: (parent, args, { me }) => {
       return me;
     },
     //On renvoie les donnée d'un user ciblé en fonction de son ID
     user: (parent, {id}) => {
       return users[id];
     },
-    
+    //On renvoie l'ensemble des donnée de la table message
+    messages: () => {
+      return Object.values(messages);
+    },
+    //On renvoie les donnée d'un message ciblé en fonction de son ID
+    message: (parent, { id }) => {
+      return messages[id];
+    },
   },
+  
   // Ici on impose a tous les usernames la valeur 'Hans'
   // User: {
   //     username: () => 'Hans',
@@ -76,16 +104,20 @@ const resolvers = {
   //     return parent.username;
   //   }
   // }
+
   //On definis ici a username un nom et prenom
-  User: {
-    username: user => `${user.firstname} ${user.lastname}`,
-  },
+  // User: {
+  //   username: user => `${user.firstname} ${user.lastname}`,
+  // },
 };
 //On lance le serveur Apollo
 const server = new ApolloServer({
   // Definitions des types -> schema et requete faite avec resolvers
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 //On utilise ici 'applyMiddleware' pour lancer express avec le serveur appollo pour lancer Graphql playground
 //On lance dans le navigateur localhost:8000/graphql pour acceder a la page de graphql playground
